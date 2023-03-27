a764091a04698279347b48f2755baa898e587b00
Francisco-Intelangelo <115087753+Francisco-Intelangelo@users.noreply.github.com>
2023-03-27 12:01:55 -0300

    Update hola_mundo.js

--- Curso-JavaScript/mi-primer-proyecto-de-node/hola_mundo.js
+++ Curso-JavaScript/mi-primer-proyecto-de-node/hola_mundo.js
@@ -1,1 +1,13 @@
 console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo");
+
+nombre = "Francisco";
+edad = 24;
+desarrollador = true;
+fecha_nacimiento = new Date("Jan 21 1999");
+libro = {
+    titulo : "No Tengo pero bueno",
+    autor : "Ni idea",
+    fecha : new Date("Jun 05 2016")
+}
+lista = { nombre,edad,desarrollador,fecha_nacimiento,libro}
+console.log(lista)
