012727f75a8eee1261ef1f98922139e1172b4a7d
Francisco-Intelangelo <115087753+Francisco-Intelangelo@users.noreply.github.com>
2023-04-04 09:34:21 -0300

    Add files via upload

---
+++ Curso-JavaScript/mi-primer-proyecto-de-node/ejercicios/ej5.js
@@ -0,0 +1,10 @@
+alturaCM = 170;
+alturaMetros = 1.70;
+peso = 72;
+alturaMR = 2;
+pesoRA = 70;
+valorMax1 = Infinity +1;
+valorMax = Infinity;
+if(valorMax === valorMax1){
+    console.log("El valor es el mismo" + valorMax1);
+}
